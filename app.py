a3d52459e2e087feebc6d4fe51197768885fdae3
Lise Styve <s153748@student.dtu.dk>
2021-04-20 10:44:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 import dash
-import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
 
-# Initiate the app
-app = dash.Dash(
-    external_stylesheets=[dbc.themes.BOOTSTRAP]
-)
+# Define variables
+tabtitle='events'
+myheading='Extreme Weather Detection'
+githublink='https://github.com/s153748/extreme-weather-detection'
+
+# Set up visualization
+
+# Initiate app
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+app.title = tabtitle
 
 # Set up the layout
-app.layout = dbc.Alert(
-    "Test", className="m-5"
+app.layout = html.Div(children=[
+    html.H1(myheading),
+    html.A('Code on Github', href=githublink),
+    html.Br(),
+    ]
 )
 
 if __name__ == '__main__':
