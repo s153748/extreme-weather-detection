3ed2c1ef2502fe41de305eb6bedf9093b78d47a9
Lise Styve <s153748@student.dtu.dk>
2021-04-25 16:58:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -171,13 +171,15 @@ app.layout = html.Div(
             className="banner",
             children=[
                 html.H6("Extreme Weather Event Detection"),
+                html.Img(src=app.get_asset_url("GitHub-Mark-Light-64px.png",
+                         style={'height':'110%', 'width':'110%'}),
                 html.A(id='gh-link',
                        children=['View on Github'], 
                        href=githublink, 
-                       style={'color': 'white', 'border': 'solid 1px white'}),
-                html.Img(src=app.get_asset_url("GitHub-Mark-Light-64px.png"))
-            ],
-        ),
+                       style={'color': 'white', 'border': 'solid 1px white', 'font-size':'12px'}
+                )
+            ]
+        )
         html.Div(
             id="upper-container",
             className="row",
