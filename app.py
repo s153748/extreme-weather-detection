136616507f1be097893049124d2a06f45d809dac
Lise Styve <s153748@student.dtu.dk>
2021-06-05 12:55:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -384,10 +384,10 @@ app.layout = dbc.Container([
                                 min=init_start,
                                 max=init_end, 
                                 value=[init_start, init_end],
-                                marks=get_marks(df['date'].min()-relativedelta(days=17), df['date'].max()+relativedelta(days=8)),
+                                marks=get_marks(df['date'].min()-relativedelta(days=17), df['date'].max()+relativedelta(days=9)),
                                 updatemode='mouseup',
                             ), 
-                        ], style={'margin-left':10}
+                        ], style={'margin-left':10, 'margin-right':10}
                     ),
                     html.Div([
                         dcc.Loading(children=dcc.Graph(
